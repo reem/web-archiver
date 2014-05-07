0c6face375f127775b2a9c868f8412c827d8bf95
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-07 15:42:01 -0700

    Crated fetcher script using async.js

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,18 @@
-// eventually, you'll have some code here that uses the code in `archive-helpers.js`
-// to actually download the urls you want to download.
+var _ = require('underscore');
+var async = require('async');
+var fs = require('fs');
+var path = require('path');
+var archive = require('../helpers/archive-helpers.js');
+var universal = require('../helpers/universal-helpers.js');
+
+// Run this immediately and don't leak variables.
+(function () {
+  // Get our urls.
+  archive.readListOfUrls(function (urls) {
+    // Get rid of the ones we already archived.
+    async.reject(urls, archive.isUrlArchived, function (results) {
+      // Download all the others.
+      async.each(results, archive.downloadUrl);
+    });
+  });
+}());
